4782d8600cdffa1ef7c62b56e0ff4c102ec563ba
Matt Schwartz <mattschwartz310@gmail.com>
2018-03-29 16:42:36 -0700

    add customer message router

--- src/routing/CustomerMessageRouter.ts
+++ src/routing/CustomerMessageRouter.ts
@@ -1,9 +1,28 @@
-import { Session } from 'botbuilder';
+import { IAddress, Session, UniversalBot } from 'botbuilder';
+import { ConversationState } from '../conversation/ConversationState';
+import { IConversationProvider } from './../conversation/IConversationProvider';
 import { IRouter } from './IRouter';
 
-export class CustomerMessageRouter implements IRouter {
-    //tslint:disable-next-line
-    public route(session: Session,  next?: Function): Promise<any> {
+export class CustomerMessageRouter<T extends IAddress> implements IRouter {
+    private readonly bot: UniversalBot;
+    private readonly convoProvider: IConversationProvider<T>;
 
+    public constructor(bot: UniversalBot, convoProvider: IConversationProvider<T>) {
+        this.bot = bot;
+        this.convoProvider = convoProvider;
+    }
+
+    //tslint:disable-next-line
+    public async route(session: Session,  next?: Function): Promise<any> {
+        const customerAddress = session.message.address;
+        const convo = await this.convoProvider.getConversationFromCustomerAddress(customerAddress);
+
+        await this.convoProvider.addCustomerMessageToTranscript(session.message);
+
+        if (convo.conversationState === ConversationState.Bot) {
+            return next();
+        } else {
+            session.send('please hold while we connect you to the next available agent');
+        }
     }
 }
