34f4215c83f8df4ae82c07d77446f62840d52236
Matt Schwartz <mattschwartz310@gmail.com>
2018-03-29 17:43:02 -0700

    add apply handoff function

--- src/middleware/applyHandoff.ts
+++ src/middleware/applyHandoff.ts
@@ -1,10 +1,15 @@
 import { IAddress, UniversalBot } from 'botbuilder';
 import { InMemoryConversationProvider } from '../conversation/prebuiltProviders/InMemoryConversationProvider';
 import { IConversationProvider } from './../conversation/IConversationProvider';
+import { IncomingMessageRouter } from './../routing/IncomingMessageRouter';
 
-export function applyHandoffMiddleware<T extends IAddress>(
+export function applyHandoff<T extends IAddress>(
     bot: UniversalBot,
     provider: IConversationProvider<T> = new InMemoryConversationProvider()
 ): void {
+    const incomingMessageRouter = new IncomingMessageRouter(bot, provider);
 
+    bot.use({
+        botbuilder: incomingMessageRouter.route
+    });
 }
